5d488ca038fb4c747d89bf6e6edca9deb83985a8
Karla <kescudero.a@gmail.com>
2022-10-06 19:38:50 -0300

    add view home and css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import NavBar from './components/Navbar';
-
+import Home from './views/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-
+      <Route path="/" element={<Home />}/>
+       
       </Routes>
     </BrowserRouter>
   );
