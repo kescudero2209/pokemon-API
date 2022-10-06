58abf6f3e832c384755b1a56aa713e43fae66e3c
Karla <kescudero.a@gmail.com>
2022-10-06 19:42:53 -0300

    add view details pokemon

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from './components/Navbar';
 import Home from './views/Home';
 import SearchPokemon from './views/SearchPokemon';
+import DetailsPokemon from './views/DetailsPokemon';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/pokemon" element={ <SearchPokemon />}/>
-       
+      <Route path="/pokemon/:name" element={ <DetailsPokemon />}/>
+
       </Routes>
     </BrowserRouter>
   );
